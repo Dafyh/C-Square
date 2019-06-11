6b26d49662a2ec5e38d5623cad69429aa3df3b0a
Nicolas Marteau <nico_mart@hotmail.fr>
2019-06-11 10:36:59 +0200

    chore: add mowersAnalyser func

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Require Node.js dependencies
-const { readFile } = require("fs").promises;
+const { readFileSync } = require("fs");
 const { join } = require("path");
 const { EOL } = require("os");
 
@@ -10,17 +10,79 @@ const { cyan } = require("kleur");
 const GRID = [];
 
 /**
- * @async
- * @func main
- * @desc Parse & calculate mower trajectory
+ * @func mowerAnalyser
+ * @desc Calculate mower trajectory
+ * @param {string[]} mower Array with initial position & sequence of the mower
+ * @param {number} index Index of the array
  * @returns {void}
- * @throws Error
+ * @throws
+ */
+function mowerAnalyser(mower, index) {
+    const letConfig = new Set("NESWLRF");
+    const message = "Configuration must contain the following letters : N-E-S-W and L-R-F. Check your config.cfg file.";
+    const [ position, sequence ] = mower;
+    const [ posMaxX, posMaxY ] = GRID;
+    let [ x, y, orient] = position.split(" ");
+
+    // Check config
+    if (!letConfig.has(orient) || !sequence.split("").every((letter) => letConfig.has(letter))) {
+        throw new Error(message);
+    }
+
+    for (const action of sequence) {
+        switch (action) {
+            case "F":
+                if (orient === "N") {
+                    if (y === posMaxY) break;
+                    y++;
+                }
+                if (orient === "E") {
+                    if (x === posMaxX) break;
+                    x++;
+                }
+                if (orient === "S") {
+                    if (y === 0) break;
+                    y--;
+                }
+                if (orient === "W") {
+                    if (x === 0) break;
+                    x--;
+                }
+                break;
+            
+            case "L":
+                if (orient === "N") { orient = "W"; break; }
+                if (orient === "W") { orient = "S"; break; }
+                if (orient === "S") { orient = "E"; break; }
+                if (orient === "E") { orient = "N"; break; }
+                break;
+
+            case "R":
+                if (orient === "N") { orient = "E"; break; }
+                if (orient === "E") { orient = "S"; break; }
+                if (orient === "S") { orient = "W"; break; }
+                if (orient === "W") { orient = "N"; break; }
+                break;
+
+            default:
+                break;
+        }
+    }
+    // Log result
+    const initialPos = `Initial position : ${cyan(position)}  / `
+    const prog = `Sequence : ${cyan(sequence)}  / `
+    const out = `Output : ${cyan([x, y, orient].join(" "))}`
+    console.log(`Mower ${index + 1} ==>`, initialPos, prog, out);
+}
+
+/**
+ * @func main
+ * @desc Parse config.cfg & start mowerAnalyser
+ * @returns {void}
  */
 async function main() {
     // Filters datas of the config file
-    const letConfig = new Set("NESWLRF");
-    const message = "Configuration must contain the following letters : N-E-S-W and L-R-F. Check your config.cfg file.";
-    const fileConfig = await readFile(join(__dirname, "config.cfg"), { encoding: "utf8" });
+    const fileConfig = readFileSync(join(__dirname, "config.cfg"), { encoding: "utf8" });
     const [ gridLimit, ...mowersConfig ] = fileConfig.toUpperCase().split(EOL);
     GRID.push(...gridLimit.split(" "));
 
@@ -34,60 +96,7 @@ async function main() {
     }
 
     // Loop on each mower
-    for (const [ position, sequence ] of mowers) {
-        let [ x, y, orient] = position.split(" ");
-        const [ posMaxX, posMaxY ] = GRID;
-
-        // Check config
-        if (!letConfig.has(orient) || !sequence.split("").every((letter) => letConfig.has(letter))) {
-            throw new Error(message);
-        }
-
-        for (const action of sequence) {
-            switch (action) {
-                case "F":
-                    if (orient === "N") {
-                        if (y === posMaxY) break;
-                        y++;
-                    }
-                    if (orient === "E") {
-                        if (x === posMaxX) break;
-                        x++;
-                    }
-                    if (orient === "S") {
-                        if (y === 0) break;
-                        y--;
-                    }
-                    if (orient === "W") {
-                        if (x === 0) break;
-                        x--;
-                    }
-                    break;
-                
-                case "L":
-                    if (orient === "N") { orient = "W"; break }
-                    if (orient === "W") { orient = "S"; break }
-                    if (orient === "S") { orient = "E"; break }
-                    if (orient === "E") { orient = "N"; break }
-                    break;
-
-                case "R":
-                    if (orient === "N") { orient = "E"; break }
-                    if (orient === "E") { orient = "S"; break }
-                    if (orient === "S") { orient = "W"; break }
-                    if (orient === "W") { orient = "N"; break }
-                    break;
-
-                default:
-                    break;
-            }
-        }
-        // Log result
-        const initialPos = `Initial position : ${cyan(position)}  / `
-        const prog = `Sequence : ${cyan(sequence)}  / `
-        const out = `Output : ${cyan([x, y, orient].join(" "))}`
-        console.log("Mower 1 ==>", initialPos, prog, out);
-    }
+    mowers.map(mowerAnalyser);
 }
 
 main().catch(console.error);
